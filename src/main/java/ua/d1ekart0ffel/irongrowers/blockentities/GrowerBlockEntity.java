14a62a25d1dc38dd4b80a33ec1d7f070139eeca5
dieKartoffel <takinkgo@gmail.com>
2024-08-08 00:30:39 +0300

    Updated scanning process

--- src/main/java/ua/d1ekart0ffel/irongrowers/blockentities/GrowerBlockEntity.java
+++ src/main/java/ua/d1ekart0ffel/irongrowers/blockentities/GrowerBlockEntity.java
@@ -3,7 +3,6 @@ package ua.d1ekart0ffel.irongrowers.blockentities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.ParticleUtils;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -26,26 +25,7 @@ public class GrowerBlockEntity extends BlockEntity {
 
         ServerLevel server = (ServerLevel) level;
 
-        int x = pos.getX();
-        int y = pos.getY();
-        int z = pos.getZ();
-
-        int x1 = x + radius + 1;
-        int z1 = z + radius;
-        int x2 = x - radius;
-        int z2 = z - radius;
-
-        for (int i = 0; i < Math.pow(1 + radius * 2, 2); i++) {
-
-            BlockPos blockScanPos = null;
-            if (x1 > x2) {
-                x1--;
-                blockScanPos = new BlockPos(x1, y, z1);
-            } else if (z1 > z2) {
-                z1--;
-                x1 = x + radius;
-                blockScanPos = new BlockPos(x1, y, z1);
-            }
+        for (BlockPos blockScanPos : BlockPos.betweenClosed(pos.offset(-4, 0, -4), pos.offset(4, 0, 4))) {
             if (blockScanPos != null && level.getRandom().nextInt(100) <= growthChance) {
                 BlockState cropstate = level.getBlockState(blockScanPos);
 
@@ -55,7 +35,7 @@ public class GrowerBlockEntity extends BlockEntity {
 
                     if (age < maxAge) {
                         level.setBlock(blockScanPos, cropBlock.getStateForAge(age + 1), 2);
-                        server.sendParticles(ParticleTypes.HAPPY_VILLAGER, blockScanPos.getX()+0.5, blockScanPos.getY()+0.5,blockScanPos.getZ()+0.5, 5, 0.25, 0.25, 0.25, 0.1);
+                        server.sendParticles(ParticleTypes.HAPPY_VILLAGER, blockScanPos.getX() + 0.5, blockScanPos.getY() + 0.5, blockScanPos.getZ() + 0.5, 5, 0.25, 0.25, 0.25, 0.1);
                     }
                 }
             }
