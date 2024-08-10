c67667d5a3315a007a42eed2d4537fa6b01a4e26
dieKartoffel <takinkgo@gmail.com>
2024-08-10 15:14:43 +0300

    Optimised WaterMixin

--- src/main/java/ua/d1ekart0ffel/irongrowers/mixin/WaterMixin.java
+++ src/main/java/ua/d1ekart0ffel/irongrowers/mixin/WaterMixin.java
@@ -1,28 +1,25 @@
 package ua.d1ekart0ffel.irongrowers.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.FarmBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FluidState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
 import ua.d1ekart0ffel.irongrowers.blocks.GrowerBlock;
 
 @Mixin(FarmBlock.class)
 public abstract class WaterMixin {
-    /**
-     * @author d1eKart0ffel
-     * @reason In order to make FarmLand check nearby for GrowerBlock and if true turn FarmLand state to .MOISTURE (in randomTick method)
-     */
-    @Overwrite
-    private static boolean isNearWater(LevelReader level, BlockPos pos) {
-        BlockState state = level.getBlockState(pos);
-        for (BlockPos blockpos : BlockPos.betweenClosed(pos.offset(-4, 0, -4), pos.offset(4, 1, 4))) {
-            if (state.canBeHydrated(level, pos, level.getFluidState(blockpos), blockpos) || level.getBlockState(blockpos).getBlock() instanceof GrowerBlock) {
-                return true;
-            }
-        }
-        return net.neoforged.neoforge.common.FarmlandWaterManager.hasBlockWaterTicket(level, pos);
+
+    @WrapOperation(
+            method = "isNearWater",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;canBeHydrated(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/material/FluidState;Lnet/minecraft/core/BlockPos;)Z")
+    )
+    private static boolean isGrowerBlock(BlockState instance, BlockGetter blockGetter, BlockPos pos, FluidState fluidState, BlockPos blockPos, Operation<Boolean> original) {
+        return original.call(instance, blockGetter, pos, fluidState, blockPos) || blockGetter.getBlockState(blockPos).getBlock() instanceof GrowerBlock;
     }
 }
 
