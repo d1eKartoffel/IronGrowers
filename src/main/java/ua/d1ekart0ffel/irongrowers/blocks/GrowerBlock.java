0b88b32cc0ab95859e4ec28664791eaee012c164
dieKartoffel <takinkgo@gmail.com>
2024-08-16 18:16:16 +0300

    - Added opening Grower Block menu on click - Added dropping processing of Grower Block contents - Added renderer

--- src/main/java/ua/d1ekart0ffel/irongrowers/blocks/GrowerBlock.java
+++ src/main/java/ua/d1ekart0ffel/irongrowers/blocks/GrowerBlock.java
@@ -1,20 +1,25 @@
 package ua.d1ekart0ffel.irongrowers.blocks;
 
+import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.Containers;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
 import ua.d1ekart0ffel.irongrowers.blockentities.GrowerBlockEntity;
-import ua.d1ekart0ffel.irongrowers.mixin.WaterMixin;
 import ua.d1ekart0ffel.irongrowers.registry.BlockEntityRegistry;
-import ua.d1ekart0ffel.irongrowers.registry.BlockRegistry;
 
-public class GrowerBlock extends Block implements EntityBlock {
+public class GrowerBlock extends BaseEntityBlock {
 
     public GrowerBlock(Properties properties, double growthInterval, int radius, int growthChance) {
         super(properties);
@@ -27,20 +32,61 @@ public class GrowerBlock extends Block implements EntityBlock {
     private final int radius;
     private final int growthChance;
 
+    @Override
+    protected MapCodec<? extends BaseEntityBlock> codec() {
+        return null;
+    }
+
+    @Override
+    public RenderShape getRenderShape(BlockState pState) {
+        return RenderShape.MODEL;
+    }
+
+    //Open menu on click
+    protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult) {
+        if (level.isClientSide) {
+            return InteractionResult.SUCCESS;
+        } else {
+            this.openContainer(level, pos, player);
+            return InteractionResult.CONSUME;
+        }
+    }
+
+    //Open menu
+    protected void openContainer(Level level, BlockPos pos, Player player) {
+        BlockEntity blockentity = level.getBlockEntity(pos);
+        if (blockentity instanceof GrowerBlockEntity) {
+            player.openMenu((MenuProvider) blockentity);
+        }
+    }
+
+    //Drop contents on remove
+    @Override
+    public void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean isMoving) {
+        if (!state.is(newState.getBlock())) {
+            BlockEntity blockEntity = level.getBlockEntity(pos);
+            if (blockEntity instanceof GrowerBlockEntity growerBlockEntity) {
+                //Drops contents
+                if (level instanceof ServerLevel) {
+                    Containers.dropContents(level, pos, growerBlockEntity.getDroppableInventory());
+                }
+                super.onRemove(state, level, pos, newState, isMoving);
+            } else {
+                super.onRemove(state, level, pos, newState, isMoving);
+            }
+        }
+    }
+
+    //Creates GrowerBlockEntity when placed
     @Nullable
     @Override
     public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
-        return new GrowerBlockEntity(blockPos, blockState);
+        return BlockEntityRegistry.GROWER_BLOCK_ENTITY.get().create(blockPos,blockState);
     }
 
     @Nullable
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
-        return getTicker(type, BlockEntityRegistry.GROWER_BLOCK_ENTITY.get(), (level1, pos, state1, blockEntity) -> GrowerBlockEntity.tick(level1, pos, state1, blockEntity, growthInterval, radius, growthChance));
-    }
-
-    @Nullable
-    public static <A extends BlockEntity, T extends BlockEntity> BlockEntityTicker<T> getTicker(BlockEntityType<T> innerType, BlockEntityType<A> targetType, BlockEntityTicker<? super A> ticker) {
-        return targetType == innerType ? (BlockEntityTicker<T>) ticker : null;
+        return level.isClientSide ? null : createTickerHelper(type, BlockEntityRegistry.GROWER_BLOCK_ENTITY.get(), (level1, pos, state1, blockEntity) -> GrowerBlockEntity.serverTick((ServerLevel) level1, pos, state1, blockEntity, growthInterval, radius, growthChance));
     }
 }
